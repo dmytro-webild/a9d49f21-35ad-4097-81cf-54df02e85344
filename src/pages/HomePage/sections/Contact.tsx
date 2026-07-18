298002f09c1421cf1561c24f5f08ddf213a38216
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-07-18 03:05:54 +0000

    Bob AI: Set image to https://storage.googleapis.com/webild/users/user_3GMdm7yk6wEN7W9hhW

--- src/pages/HomePage/sections/Contact.tsx
+++ src/pages/HomePage/sections/Contact.tsx
@@ -7,7 +7,7 @@ export default function ContactSection() {
       <div className="w-content-width mx-auto flex justify-center">
         <div className="w-full max-w-4xl rounded-xl overflow-hidden shadow-2xl">
           <ImageOrVideo 
-            imageSrc="https://storage.googleapis.com/webild/users/user_3GMdm7yk6wEN7W9hhWl6vUrowgd/uploaded-1783860744795-c29vvtqc.png"
+            imageSrc="https://storage.googleapis.com/webild/users/user_3GMdm7yk6wEN7W9hhWl6vUrowgd/uploaded-1784343928035-3vdk2n1b.png"
             className="w-full h-auto"
           />
         </div>
